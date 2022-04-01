2de960a651a5d203df0c4a7954e7ff0b6b07ce09
Aderson Bertim <adersonbertim@gmail.com>
2022-03-31 21:06:22 -0300

    variables 1

---
+++ Scripts/variables.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+name="aderson"
+#We have a variable assing my name. IMPORTANT: Dont have any space with variable and value.
+
+echo ${name} 
+#Here we have a echo command with the variable and this brackets its a good practice.
