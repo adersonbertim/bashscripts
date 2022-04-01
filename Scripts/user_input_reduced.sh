36cb1c4ec71573aeae03654d700e4085e66034c4
Aderson Bertim <adersonbertim@gmail.com>
2022-03-31 21:45:28 -0300

    Reduced form to write this code

---
+++ Scripts/user_input_reduced.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+read -p "Hey what's your age?" age
+# That tag -p display in prompt output the string PROMPT whithout a trailing newline before attempting to read.
+# After this end of string we have a variable age.
+
+echo "WOWWW you have $age years old awesome!!"
+# Here the echo read the variable and display on prompt
+
