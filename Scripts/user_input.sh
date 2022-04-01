0a1e82c3095f69746b4d5e14a593f55a46136d7e
Aderson Bertim <adersonbertim@gmail.com>
2022-03-31 21:37:00 -0300

    This is a simple user input

---
+++ Scripts/user_input.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Heyy bro what's your name and age?"
+# This echo i will put a simple phrase and after..
+
+read name
+read age
+# This comand read this variables
+
+echo "Hi ${name}, oww you have ${age} years old!!"
+# And here i print this variables and have a reduced form.
+
