0eac88cdfd88e3d5dc10bb6d7d59fb87c2a62384
Aderson Bertim <adersonbertim@gmail.com>
2022-06-20 13:17:17 -0300

    add: new color

--- Mini Projects/password_gen.sh
+++ Mini Projects/password_gen.sh
@@ -9,7 +9,7 @@ read -p "How many characters you like in your password? " pass_length
 printf "\n"
 
 # Script to generate a password.
-for i in {1..5}; do (tr -cd '[:alnum:]' < /dev/urandom | fold -w${pass_length} | head -n 1); done
+for i in {1..5}; do (tr -cd '[:alnum:]' < /dev/urandom | fold -w"$pass_length" | head -n 1); done
 # We have here a loop with cat the path in dev/urandom and add a variable pass_length and print then.
 
 # We have other ways to do that like ..
@@ -17,4 +17,4 @@ for i in {1..5}; do (tr -cd '[:alnum:]' < /dev/urandom | fold -w${pass_length} |
 
 # We print the password.
 printf "$pass_output\n"
-printf "That are a password suggestions for you ${USER} have a nice day! :)\n"
+printf "That are a password suggestions for you $USER have a nice day! :)\n"
